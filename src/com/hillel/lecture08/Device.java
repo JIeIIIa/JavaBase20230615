3dbdd9274c3072526d395c43179d61783e9272d9
Oleksii Onishchenko <stealth@bigmir.net>
2023-07-10 20:29:13 +0300

    Lecture-08 - Device class:constructors

--- src/com/hillel/lecture08/Device.java
+++ src/com/hillel/lecture08/Device.java
@@ -2,21 +2,37 @@ package com.hillel.lecture08;
 
 public class Device {
     String imei = "1111";
+    String color;
 
     public static void main(String[] args) {
-        Device device = new Device();
+        String test = "test";
+        System.out.println(test);
+        Device device = new Device("qwerty", "white");
         device.printInfo();
-        device.imei = "qwerty";
-        device.printInfo();
+//        device.printInfo();
 
-        Device anotherDevice = new Device();
-        anotherDevice.imei = "2222222";
+        Device anotherDevice = new Device("222222");
         anotherDevice.printInfo();
-        device.printInfo();
+    }
+
+    public Device() {
+        System.out.println("Constructor with NO parameters");
+    }
+
+    public Device(String imei) {
+        this();
+        System.out.println("Constructor with String parameter");
+        this.imei = imei;
+    }
+
+    public Device(String imei, String color) {
+        this.imei = imei;
+        this.color = color;
     }
 
     public void printInfo() {
         System.out.println("General info: imei " + imei);
+        System.out.println("\t\tcolor " + color);
     }
 
     public void printInfo(String prefix) {
