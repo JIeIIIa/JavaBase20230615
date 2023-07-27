a84f596f0faf56676f36ff67d4328e3742e3afee
Oleksii Onishchenko <stealth@bigmir.net>
2023-07-27 20:08:07 +0300

    Lecture-13 - Switch with enum

--- src/main/java/com/hillel/lecture13/DaysOfWeek.java
+++ src/main/java/com/hillel/lecture13/DaysOfWeek.java
@@ -1,25 +1,39 @@
 package com.hillel.lecture13;
 
+import static com.hillel.lecture13.Day.MONDAY;
+import static com.hillel.lecture13.Day.WEDNESDAY;
+
 public class DaysOfWeek {
     public static void main(String[] args) {
         DaysOfWeek daysOfWeek = new DaysOfWeek();
 
-        String message = daysOfWeek.giveRecommendation(Day.WEDNESDAY);
+        String message = daysOfWeek.giveRecommendation(MONDAY);
 
         System.out.println(message);
     }
 
     public String giveRecommendation(Day day) {
         String message = "";
-        if (day == Day.WEDNESDAY) {
+        if (day == WEDNESDAY) {
             message += "Listen music\n";
         }
-        if (day.isWorkingDay()) {
-            message += String.format("Today is %s. Don't forget to see a meeting list", day.getName());
-        } else {
-            message += String.format("Today is weekend [%s]. Go to park", day.getName());
+        switch (day) {
+            case MONDAY:
+                message += buildWorkingDayMessage(day, message);
+                break;
+            case WEDNESDAY:
+                message += buildWorkingDayMessage(day, message);
+                break;
+            case SUNDAY:
+                message += String.format("Today is weekend [%s]. Go to park\n", day.getName());
+
         }
 
+        return message;
+    }
+
+    private static String buildWorkingDayMessage(Day day, String message) {
+        message += String.format("Today is %s. Don't forget to see a meeting list\n", day.getName());
         return message;
     }
 }
