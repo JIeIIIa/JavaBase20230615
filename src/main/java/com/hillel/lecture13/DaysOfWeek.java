2245a4d93cc2a06fa075dc3101b2cb93d5d6012d
Oleksii Onishchenko <stealth@bigmir.net>
2023-07-27 19:44:05 +0300

    Lecture-13 - No enum (problems): trivial implementation - 2

--- src/main/java/com/hillel/lecture13/DaysOfWeek.java
+++ src/main/java/com/hillel/lecture13/DaysOfWeek.java
@@ -3,30 +3,51 @@ package com.hillel.lecture13;
 public class DaysOfWeek {
     public static void main(String[] args) {
         DaysOfWeek daysOfWeek = new DaysOfWeek();
-        Day monday = new Day(-1);
-        String message = daysOfWeek.giveRecommendation(monday);
+
+        String message = daysOfWeek.giveRecommendation(Day.WEDNESDAY);
 
         System.out.println(message);
     }
 
     public String giveRecommendation(Day day) {
+        if (day == Day.WEDNESDAY) {
+            System.out.println("Listen music");
+        }
         if (day.isWorkingDay()) {
-            return "Don't forget to see a meeting list";
+            return String.format("Today is %s. Don't forget to see a meeting list", day.getName());
         } else {
-            return "Go to park";
+            return String.format("Today is weekend [%s]. Go to park", day.getName());
         }
 
     }
 }
 
 class Day {
-    private int numberOfDay;
+    public static final Day MONDAY = new Day(1, "Monday");
+    public static final Day WEDNESDAY = new Day(1, "Wednesday");
+    public static final Day SUNDAY = new Day(7, "Sunday");
 
-    public Day(int numberOfDay) {
+    private int numberOfDay;
+    private String name;
+
+    private Day(int numberOfDay) {
         if (numberOfDay < 1 || numberOfDay > 7) {
             throw new RuntimeException("ERROR! Unknown day");
         }
         this.numberOfDay = numberOfDay;
+    }
+
+    private Day(int numberOfDay, String name) {
+        this(numberOfDay);
+        this.name = name;
+    }
+
+    public int getNumberOfDay() {
+        return numberOfDay;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public boolean isWorkingDay() {
